206617f6f816e6ca8976d8955d953f97f31f9faf
Kara Bernert <krbernert@gmail.com>
2014-09-08 15:47:49 -0400

    fetch current user on sign in

--- app/assets/javascripts/ok_clone.js
+++ app/assets/javascripts/ok_clone.js
@@ -16,12 +16,12 @@ window.OKC = {
     OKC.responses = new OKC.Collections.Responses();
     OKC.users = new OKC.Collections.Users();
 
-    var user = OKC.current_user = new OKC.Models.User({ id: user_id });
+    OKC.current_user = new OKC.Models.User({ id: user_id });
+
 
     new OKC.Routers.Main({$rootEl : $("main")});
     Backbone.history.start();
-
-    console.log("backbone runs");
+    OKC.current_user.fetch();
   }
 };
 
